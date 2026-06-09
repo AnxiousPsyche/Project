d18e5cc546d8a0601178a2ed6847e94382d99d5f
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-06-09 22:40:33 +0800

    Javascript CC part 3

--- Javascript Crash Course/main.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById('myH1').textContent = 'Hello World';
-// document.getElementById('myP').textContent = 'Hi';
-
-let firstName = "Leah Almazan";
-let favoriteFood = "Burger";
-
-console.log(typeof firstName);
-console.log(`Your name is ${firstName}`);
-console.log(`You like ${favoriteFood}`);
