fefc807155296f97643e7922f617c91b06febf8e
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-06-09 09:44:16 +0800

    Javascript Crash Course part 2

---
+++ Javascript Crash Course/main.js
@@ -0,0 +1,9 @@
+// document.getElementById('myH1').textContent = 'Hello World';
+// document.getElementById('myP').textContent = 'Hi';
+
+let firstName = "Leah Almazan";
+let favoriteFood = "Burger";
+
+console.log(typeof firstName);
+console.log(`Your name is ${firstName}`);
+console.log(`You like ${favoriteFood}`);
