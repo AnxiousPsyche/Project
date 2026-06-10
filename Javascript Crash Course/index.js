58d84753cb7494329157608ff364340322ea7e47
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-06-10 15:49:10 +0800

    Counter Program JS

--- Javascript Crash Course/index.js
+++ Javascript Crash Course/index.js
@@ -1,10 +1,20 @@
-//let username ;
-//username = window.prompt ("What's your username");
-//console.log(username);
 
-let username;
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
 
-document.getElementById("mySubmit").onclick = function (){
-    username = document.getElementById ("myText").value;
-    console.log(username);}
+increaseBtn.onclick= function(){
+    count++;
+    countLabel.textContent = count;
+}
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
 
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
