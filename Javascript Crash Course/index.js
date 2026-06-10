0017a3974f9437efc3e3f6b371529dbed2690e38
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-06-10 14:41:36 +0800

    Accept user input JS

--- Javascript Crash Course/index.js
+++ Javascript Crash Course/index.js
@@ -1,12 +1,10 @@
-let fullName = "Leah Almazan" ;
-let age = 30 ;
-let isStudent = false ;
+//let username ;
+//username = window.prompt ("What's your username");
+//console.log(username);
 
+let username;
 
-document.getElementById("p1").textContent = `Your name is ${fullName}` ;
-document.getElementById("p2").textContent = `You are ${age} years old` ;
-document.getElementById("p3").textContent = `Enrolle: ${isStudent}`;
-
-let result = 6 / 2 ** (2+5);
-console.log(result)
+document.getElementById("mySubmit").onclick = function (){
+    username = document.getElementById ("myText").value;
+    console.log(username);}
 
