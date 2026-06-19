95a595e89d3263c1a6a732573f7dcb3df7bb4e2b
AnxiousPsyche <almazan.leahlma@gmail.com>
2026-06-19 23:14:32 +0800

    My Project added hover

--- Javascript Crash Course/Ternary Operators/index.js
+++ Javascript Crash Course/Ternary Operators/index.js
@@ -1,7 +1,7 @@
 let age = Number(prompt("Please enter your age:"));
 let message = 
-            age >= 18 ? "Congrats! you are an adult."
-            : age == 100 ? "Wow! you are a centenarian!" 
+            age >= 100 ? "Wow! you are a centenarian!" 
+            :age >=18 ? "Congrats! you are an adult."
             : age <= 0 ? "You haven't been conceived yet!"
             : "Sorry, you are not an adult yet.";
 prompt(message);
--- my_project/index.js
+++ my_project/index.js
@@ -1,17 +1,25 @@
 const darkModeToggle = document.getElementById("themeIcon");
 const themeIcon = document.querySelector("#themeIcon i");
+const heroImage = document.getElementById("heroID");
+let isDarkMode = false;
 
-darkModeToggle.onclick = function() {
-    
-    if(document.body.style.backgroundColor === "black") {
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
+darkModeToggle.onclick = function () {
+    if (isDarkMode) {
+        document.body.style.backgroundColor = "#fafafa";
+        document.body.style.color = "#2C2C2C";
         themeIcon.classList.remove("bi-brightness-high-fill");
         themeIcon.classList.add("bi-moon-fill");
+        heroImage.src="/web design ideas/HeroID.jpg";
+        
+        isDarkMode = false;
+
     } else {
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "white";
+        document.body.style.backgroundColor = "#2C2C2C";
+        document.body.style.color = "#fafafa";
         themeIcon.classList.remove("bi-moon-fill");
         themeIcon.classList.add("bi-brightness-high-fill");
-    }   
+        heroImage.src="/web design ideas/HeroIDDark1.jpg";
+        
+        isDarkMode = true;
+    }
 }
